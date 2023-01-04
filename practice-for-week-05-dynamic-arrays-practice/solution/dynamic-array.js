5093b3aac2c10f1dff46d0bac578075abfac2af6
jagLas <jglasher@gmail.com>
2023-01-04 13:32:30 -0500

    constructor finished

--- practice-for-week-05-dynamic-arrays-practice/solution/dynamic-array.js
+++ practice-for-week-05-dynamic-arrays-practice/solution/dynamic-array.js
@@ -1,9 +1,13 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
+    this.capacity = defaultSize;
+    this.data = [];
+    this.length = 0;
 
-    // Your code here
-
+    for (let i = 0; i < defaultSize; i++) {
+      this.data.push('');
+    }
   }
 
   read(index) {
@@ -17,6 +21,11 @@ class DynamicArray {
   }
 
 }
+
+dynamicArr = new DynamicArray();
+console.log(dynamicArr.length)
+console.log(this.data)
+
 
 
 module.exports = DynamicArray;
