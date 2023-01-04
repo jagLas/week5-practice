3780b3bc61a380af28e5fe096c0bce0764cc79f1
jagLas <jglasher@gmail.com>
2023-01-04 15:49:29 -0500

    done phase 2

--- practice-for-week-05-linked-list-practice/doubly-linked-list.js
+++ practice-for-week-05-linked-list-practice/doubly-linked-list.js
@@ -8,15 +8,56 @@ class DoublyLinkedListNode {
 
 class DoublyLinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
+    //increase length of linked list
+    this.length++
+
+    //constructs new node and sets next value to the head. Tail should remain null
+    const newNode = new DoublyLinkedListNode(val);
+    newNode.next = this.head;
+
+    //if list empty, newNode should become new tail and head
+    if(!this.head) {
+      this.tail = newNode;
+      this.head = newNode;
+      return;
+    }
+
+    //if it isnt empty it should:
+    //set the old head's previous to the new node
+    this.head.prev = newNode;
+    //set the new head to point to the new node
+    this.head = newNode;
+
   }
 
   addToTail(val) {
-    // Your code here
+    //increase length of list
+    this.length++;
+
+    //creates a new node with value
+    const newNode = new DoublyLinkedListNode(val);
+
+    //checks if list is empty and sets itself to head and tail if not
+    if(!this.head) {
+      this.tail = newNode;
+      this.head = newNode;
+      return;
+    }
+
+    //sets the newNodes previous as the current tail
+    newNode.prev = this.tail;
+
+    //sets the previous tail to recognize the next Node as next
+    this.tail.next = newNode;
+    
+    //sets the tail to the newNode
+    this.tail = newNode;
   }
 
   // You can use this function to help debug
