6d9c5083ad6bd227de446e3a411e913d4d30efe8
jagLas <jglasher@gmail.com>
2023-01-04 16:27:46 -0500

    finished benchmarking

--- practice-for-week-05-linked-list-practice/linked-list-timing.js
+++ practice-for-week-05-linked-list-practice/linked-list-timing.js
@@ -6,4 +6,81 @@ Construct a timing test to verify the time complexities of `addToHead` and
 `addToTail` for both singly and doubly linked lists.
 */
 
+
+//time complexity hypothesis
+//I think singlelinked will have O(1) for add to head and O(n) for tail
+//I think doublelinked will have O(1) for both
+
+//tests seem to confirm hypothesis as only single linked addToTail seemed to increase with the number of values
+
 // Your code here
+
+function benchmarkAddToHead (){
+    const list = new LinkedList();
+
+    start = Date.now();
+
+    for(let i = 0; i < 100000; i++){
+        startTime = Date.now();
+        list.addToHead(i);
+        endTime = Date.now();
+        console.log(endTime - startTime)
+        // list.print()
+    }
+
+    console.log(Date.now() - start)
+}
+
+function benchmarkAddToTail (){
+    const list = new LinkedList();
+
+    start = Date.now();
+
+    for(let i = 0; i < 300000; i++){
+        startTime = Date.now();
+        list.addToTail(i);
+        endTime = Date.now();
+        console.log(endTime - startTime)
+        // list.print()
+    }
+
+    console.log(Date.now() - start)
+}
+
+function benchmarkAddToHeadDouble (){
+    const list = new DoublyLinkedList();
+
+    start = Date.now();
+
+    for(let i = 0; i < 300000; i++){
+        startTime = Date.now();
+        list.addToHead(i);
+        endTime = Date.now();
+        console.log(endTime - startTime)
+        // list.print()
+    }
+
+    console.log(Date.now() - start)
+}
+
+function benchmarkAddToTailDouble (){
+    const list = new DoublyLinkedList();
+
+    start = Date.now();
+
+    for(let i = 0; i < 300000; i++){
+        startTime = Date.now();
+        list.addToTail(i);
+        endTime = Date.now();
+        console.log(endTime - startTime)
+        // list.print()
+    }
+
+    console.log(Date.now() - start)
+}
+
+// benchmarkAddToHead()
+// benchmarkAddToTail()
+
+benchmarkAddToHeadDouble();
+// benchmarkAddToTailDouble();
