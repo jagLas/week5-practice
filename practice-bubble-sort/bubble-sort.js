65f7d6d148ced192c69071ca5894b85dbbd6a4e0
jagLas <jglasher@gmail.com>
2023-01-10 13:12:17 -0500

    bubble done

--- practice-bubble-sort/bubble-sort.js
+++ practice-bubble-sort/bubble-sort.js
@@ -1,18 +1,32 @@
-
 function bubbleSort(arr) {
 
-    // Iterate through the array
+  let swapMade = false;
 
-      // If the current value is greater than its neighbor to the right
-        // Swap those values
+  // Iterate through the array
+  for (let i = 0; i < arr.length - 1; i++){
 
-        // Do not move this console.log
-        console.log(arr.join(","));
+    // If the current value is greater than its neighbor to the right  
+    if (arr[i] > arr[i + 1]) {
+      // Swap those values
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+      swapMade = true;
 
+       // Do not move this console.log
+       console.log(arr.join(","));
+    }
+  }
+
+  // Otherwise, repeat from the beginning
+
+  if (swapMade) {
+    return bubbleSort(arr);
     // If you get to the end of the array and no swaps have occurred, return
-
-    // Otherwise, repeat from the beginning
+  } else {
+    return arr;
+  }
 
 }
+
+bubbleSort([2,4,6,8,1,3,5,7,9])
 
 module.exports = bubbleSort;
